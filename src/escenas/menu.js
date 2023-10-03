8c26294c15b946ea862f2aff47128dc3b8e983fe
Carrasco Candela E R <candenwnh@gmail.com>
2023-10-02 22:54:24 -0300

    solucione el sonido

--- src/escenas/menu.js
+++ src/escenas/menu.js
@@ -46,6 +46,7 @@ export class menu extends Phaser.Scene {
       this.botoninicio = this.add.image(400,400,'boton1').setInteractive();
       this.botoninicio.on('pointerdown', () =>{
         console.log("hola");
+        this.sonido.stop();
 
         //cambiar el nombre de la escena a la que tiene q cambiar owo
         this.scene.start('Escena1');
