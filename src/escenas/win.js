a92479982040a63d3cb3e7a25e192c55cb55704d
Carrasco Candela E R <candenwnh@gmail.com>
2023-10-02 21:36:50 -0300

    merge de la escena win funciona bien, falta condicion de victoria para poder visualizarla

--- src/escenas/win.js
+++ src/escenas/win.js
@@ -2,15 +2,23 @@ export class win extends Phaser.Scene {
     constructor() {
       super({ key: 'win' });
     }
-}
+
 
 preload() {
-    this.load.image('fondowin', '../../public/img/fondowin.png');
+    this.load.image('fondowin', '../public/img/fondowin.png');
+    this.load.image('boton', '../public/img/boton.png');
 }
- // use el mismo boton para volver a jugar
-this.botoninicio = this.add.image(400,400,'boton1').setInteractive();
-      this.botoninicio.on('pointerdown', () =>{
+
+create(){
+  // use el mismo boton para volver a jugar
+  this.add.image(400,300,'fondowin');
+
+this.botonvolver = this.add.image(400,400,'boton1').setInteractive();
+      this.botonvolver.on('pointerdown', () =>{
         console.log("hola");
         //cambiar el nombre de la escena a la que tiene q cambiar owo
         this.scene.start('menu');
       });
+    }
+ 
+}
